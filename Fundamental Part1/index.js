d0e5b2654e1a6dd85fb4e9d288ad5c050e23e8b1
Mouri Nath <mourinath775948@gmail.com>
2022-01-04 22:33:35 +0600

    Fundamental

---
+++ Fundamental Part1/index.js
@@ -0,0 +1,10 @@
+alert("My Swamini is my lord Shreemati Radha Rani")
+
+let js = 'Amazing';
+if (js == 'Amazing') alert("Coding is Amazing");
+X = 40 + 8 + 23 - 10
+console.log(X);
+
+let mouri = "Me";
+console.log(mouri);
+
