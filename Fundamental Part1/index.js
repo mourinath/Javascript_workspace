0bf9ebb430a8f9ca6d76a24f1abdc13add28b434
Mouri Nath <mourinath775948@gmail.com>
2022-01-13 23:29:47 +0600

    Practise Fundamentals

--- Fundamental Part1/index.js
+++ Fundamental Part1/index.js
@@ -8,3 +8,43 @@ console.log(X);
 let mouri = "Me";
 console.log(mouri);
 
+//Math Operator
+let year = 2022;
+let mouriBirthYear = 1996
+Age = year - mouriBirthYear;
+console.log("Mouri Age : " + Age)
+console.log(Age * 2 + 2);
+AgeGouri = 28;
+
+//Assignment Operator
+
+let x = 2
+x = x + 1;
+console.log(x)
+
+if (AgeGouri > Age)
+    console.log(true)
+else console.log(false);
+
+const MarksHeight = 1.524;
+const MarksMass = 52;
+
+const JhonsHeight = 1.64592;
+const JhonsMass = 58;
+
+const BMIMark = MarksHeight / (MarksMass * MarksMass)
+const BMIJhon = JhonsHeight / (JhonsMass * JhonsMass)
+console.log(MarksHeight);
+console.log(MarksMass);
+console.log(BMIMark);
+
+const markHigherBMI = (BMIJhon > BMIMark);
+console.log(markHigherBMI)
+
+var MouriJob = "Programmer";
+
+console.log("Mouri is a " + Age + " old " + MouriJob)
+
+if (Age < 25) {
+    console.log(`mouri can drive car because she is  ${Age} already`)
+}
