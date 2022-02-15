aacf1439e9e9cd0729020631a11d1f95a4071b05
Mouri Nath <mourinath775948@gmail.com>
2022-02-15 23:31:12 +0600

    update work

--- Fundamental Part1/index.js
+++ Fundamental Part1/index.js
@@ -26,6 +26,10 @@ if (AgeGouri > Age)
     console.log(true)
 else console.log(false);
 
+}
+
+/////////////------------------Coding Chalange 1
+
 const MarksHeight = 1.524;
 const MarksMass = 52;
 
@@ -41,6 +45,8 @@ console.log(BMIMark);
 const markHigherBMI = (BMIJhon > BMIMark);
 console.log(markHigherBMI)
 
+/////////////------------------Coding Chalange 1
+
 var MouriJob = "Programmer";
 
 console.log("Mouri is a " + Age + " old " + MouriJob)
@@ -48,3 +54,55 @@ console.log("Mouri is a " + Age + " old " + MouriJob)
 if (Age < 25) {
     console.log(`mouri can drive car because she is  ${Age} already`)
 }
+
+////--------------------conversion  
+
+const inputYear = 1996;
+console.log(inputYear);
+console.log(Number(inputYear))
+console.log(inputYear + 18);
+
+//////-------------------coercion
+
+let n = 10;
+n = n + 30 - '10'
+console.log(n);
+console.log(n > 23)
+
+
+////////////////////----boolean truthy and falsy
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Mouri'));
+console.log(Boolean(''));
+
+let mone = 0
+if (money) {
+    console.log("don't spend all");
+}
+else {
+    console.log("Get a job");
+}
+
+///////////////////////////----------Equlity operator
+
+const num = '23';
+const num1 = 23;
+
+if (num === num1) {
+    console.log("They're stictly equal")
+}
+else if (num == num1) {
+    console.log("They are loose equal")
+
+
+
+
+    /////---------------------Coding Chalange 2
+
+    let bill = 50, tip =
+        bill === 50 ? tip = 10 / 100;
+
+
+
